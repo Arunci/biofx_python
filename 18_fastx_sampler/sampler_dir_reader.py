cd921e189b22f722cbf92e09358548385d118f50
Arunci <fcojavier.guijarro@murciaeduca.es>
2022-08-02 16:53:53 +0200

    finish chapter 18

--- 18_fastx_sampler/sampler_dir_reader.py
+++ 18_fastx_sampler/sampler_dir_reader.py
@@ -11,7 +11,8 @@ from typing import List, NamedTuple, Optional
 
 
 class Args(NamedTuple):
-    """ Command-line arguments """
+    """Command-line arguments"""
+
     files: List[str]
     file_format: str
     percent: float
@@ -22,54 +23,62 @@ class Args(NamedTuple):
 
 # --------------------------------------------------
 def get_args() -> Args:
-    """ Get command-line arguments """
+    """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Probabilistically subset FASTA files',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Probabilistically subset FASTA files",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('-d',
-                        '--dir',
-                        metavar='DIR',
-                        type=str,
-                        nargs='+',
-                        help='Input directories of FASTA/Q file(s)')
+    parser.add_argument(
+        "-d",
+        "--dir",
+        metavar="DIR",
+        type=str,
+        nargs="+",
+        help="Input directories of FASTA/Q file(s)",
+    )
 
-    parser.add_argument('-f',
-                        '--format',
-                        help='Input file format',
-                        metavar='format',
-                        type=str,
-                        choices=['fasta', 'fastq'],
-                        default='fasta')
+    parser.add_argument(
+        "-f",
+        "--format",
+        help="Input file format",
+        metavar="format",
+        type=str,
+        choices=["fasta", "fastq"],
+        default="fasta",
+    )
 
-    parser.add_argument('-p',
-                        '--percent',
-                        help='Percent of reads',
-                        metavar='reads',
-                        type=float,
-                        default=.1)
+    parser.add_argument(
+        "-p",
+        "--percent",
+        help="Percent of reads",
+        metavar="reads",
+        type=float,
+        default=0.1,
+    )
 
-    parser.add_argument('-m',
-                        '--max',
-                        help='Maximum number of reads',
-                        metavar='max',
-                        type=int,
-                        default=0)
+    parser.add_argument(
+        "-m",
+        "--max",
+        help="Maximum number of reads",
+        metavar="max",
+        type=int,
+        default=0,
+    )
 
-    parser.add_argument('-s',
-                        '--seed',
-                        help='Random seed value',
-                        metavar='seed',
-                        type=int,
-                        default=None)
+    parser.add_argument(
+        "-s", "--seed", help="Random seed value", metavar="seed", type=int, default=None
+    )
 
-    parser.add_argument('-o',
-                        '--outdir',
-                        help='Output directory',
-                        metavar='DIR',
-                        type=str,
-                        default='out')
+    parser.add_argument(
+        "-o",
+        "--outdir",
+        help="Output directory",
+        metavar="DIR",
+        type=str,
+        default="out",
+    )
 
     args = parser.parse_args()
 
@@ -82,22 +91,24 @@ def get_args() -> Args:
     files = []
     for dirname in args.dir:
         if os.path.isdir(dirname):
-            files.extend(list(Path(dirname).rglob('*')))
+            files.extend(list(Path(dirname).rglob("*")))
 
     if not files:
-        parser.error('Found no files')
+        parser.error("Found no files")
 
-    return Args(files=files,
-                file_format=args.format,
-                percent=args.percent,
-                max_reads=args.max,
-                seed=args.seed,
-                outdir=args.outdir)
+    return Args(
+        files=files,
+        file_format=args.format,
+        percent=args.percent,
+        max_reads=args.max,
+        seed=args.seed,
+        outdir=args.outdir,
+    )
 
 
 # --------------------------------------------------
 def main() -> None:
-    """ Make a jazz noise here """
+    """Make a jazz noise here"""
 
     args = get_args()
     random.seed(args.seed)
@@ -106,17 +117,17 @@ def main() -> None:
     for i, file in enumerate(args.files, start=1):
         basename = os.path.basename(file)
         out_file = os.path.join(args.outdir, basename)
-        print(f'{i:3}: {basename}')
+        print(f"{i:3}: {basename}")
 
         ext = os.path.splitext(basename)[1]
-        fh = gzip.open(file, 'rt') if ext == '.gz' else open(file, 'rt')
-        out_fh = open(out_file, 'wt')
+        fh = gzip.open(file, "rt") if ext == ".gz" else open(file, "rt")
+        out_fh = open(out_file, "wt")
         num_taken = 0
 
         for rec in SeqIO.parse(fh, args.file_format):
             if random.random() <= args.percent:
                 num_taken += 1
-                SeqIO.write(rec, out_fh, 'fasta')
+                SeqIO.write(rec, out_fh, "fasta")
 
             if args.max_reads and num_taken == args.max_reads:
                 break
@@ -125,11 +136,13 @@ def main() -> None:
         total_num += num_taken
 
     num_files = len(args.files)
-    print(f'Wrote {total_num:,} sequence{"" if total_num == 1 else "s"} '
-          f'from {num_files:,} file{"" if num_files == 1 else "s"} '
-          f'to directory "{args.outdir}".')
+    print(
+        f'Wrote {total_num:,} sequence{"" if total_num == 1 else "s"} '
+        f'from {num_files:,} file{"" if num_files == 1 else "s"} '
+        f'to directory "{args.outdir}".'
+    )
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
